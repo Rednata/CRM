0fb050d17f8079603eb6aad06512b4e89c21de6d
Rednata <maknatosh@gmail.com>
2022-09-20 18:32:21 +0700

    mod.4_2 CRM Init

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let nameProduct = "диван";
+let quantity = 5;
+let category = "угловой";
+let price = 5000;
+
+console.log("Наименование товара:", nameProduct);
+console.log("Сумма товара:", quantity*price);
